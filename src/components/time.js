7579036f6027424cadc429e8e471f3c4f58a39cb
truphuonglong <longbaloca@gmail.com>
2019-01-05 19:06:53 +0700

    edit ui ok

--- src/components/time.js
+++ src/components/time.js
@@ -6,14 +6,15 @@ export default class Time extends React.Component {
         fields: {
             year: (new Date()).getFullYear(),
             month: (new Date().getMonth()),
-            date: (new Date().getDate()),
+            activeDate: new Date(),
             dayOfWeeks: [],
             hour: 0
         },
     }
 
     componentDidMount() {
-        const dayOfWeeks = this.calDayOfWeeks(new Date())
+        const activeDate = this.state.fields.activeDate
+        const dayOfWeeks = this.calDayOfWeeks(activeDate)
         this.setState(state => ({
             fields: {
                 ...state.fields,
@@ -58,33 +59,58 @@ export default class Time extends React.Component {
         return arrayDates;
     }
 
-    isCurrentDate = (date) => {
-        const currentDate = new Date()
-        const inputDate = new Date(date)
-        return currentDate.getFullYear() === inputDate.getFullYear() && currentDate.getMonth() === inputDate.getMonth() && currentDate.getDate() === inputDate.getDate()
+    isSameDate = (date1, date2) => {
+        const d1 = new Date(date1)
+        const d2 = new Date(date2)
+        return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate()
     }
 
-    updateDate = (symbol) => {
-        console.log(symbol)
+    setActiveDate = (activeDate, _dayOfWeeks) => {
+        const dayOfWeeks = _dayOfWeeks || this.state.fields.dayOfWeeks
+        this.setState(state => ({
+            fields: {
+                ...state.fields,
+                year: (new Date(activeDate)).getFullYear(),
+                month: (new Date(activeDate)).getMonth(),
+                activeDate: new Date(activeDate),
+                dayOfWeeks
+            }
+        }))
+    }
+
+    updateDayOfWeeks = (symbol) => {
+        //copy activeDate
+        const activeDate = new Date(this.state.fields.activeDate)
+
+        // check symbol
+        if (symbol === '-') {
+            activeDate.setDate(activeDate.getDate() - 7)
+        } else {
+            activeDate.setDate(activeDate.getDate() + 7)
+        }
+
+        //get range dateOfWeeks:
+        console.log(activeDate)
+        const dayOfWeeks = this.calDayOfWeeks(activeDate)
+        this.setActiveDate(activeDate, dayOfWeeks)
     }
 
     render() {
-        const { year, month, date, dayOfWeeks, hour } = this.state.fields
-        console.log(date, hour)
-        console.log(dayOfWeeks)
+        const { year, month, activeDate, dayOfWeeks, hour } = this.state.fields
+        console.log(activeDate)
         return (
             <div>
                 <div className="flex-horizontal">
                     <select onChange={this.onInputChanged} value={month} name="month">
                         {
-                            this.months.map((item, index) => <option value={index} key={index}>{item}</option>)
+                            this.months.map((item, index) => <option value={index} key={index} className="form-control">{item}</option>)
                         }
                     </select>
-                    <input type="number" value={year} onChange={this.onInputChanged} name="year" />
-
+                    <input type="number" value={year} onChange={this.onInputChanged} name="year" className="form-control" />
+                    <button>Save</button>
                 </div>
 
-                <table>
+                <table style={{textAlign: 'center' }}>
                     <thead>
                         <tr>
                             <th></th>
@@ -97,15 +123,22 @@ export default class Time extends React.Component {
                     <tbody>
                         <tr>
                             <td>
-                                <button onClick={() => this.updateDate('-')}><i className="fas fa-angle-left"></i></button>
+                                <button onClick={() => this.updateDayOfWeeks('-')} className="pointer"><i className="fas fa-angle-left"></i></button>
                             </td>
                             {
                                 dayOfWeeks.map((date, index) => {
-                                    return <td key={index} className={this.isCurrentDate(date) ? "dayActive" : null}>{(new Date(date)).getDate()}</td>
+                                    return (
+                                        <td
+                                            key={index}
+                                            className={`${this.isSameDate(date, new Date()) ? "current-date" : (this.isSameDate(date, activeDate) ? "date-active" : null)} pointer`}
+                                            onClick={() => this.setActiveDate(date)}>
+                                            {(new Date(date)).getDate()}
+                                        </td>
+                                    )
                                 })
                             }
                             <td>
-                                <button onClick={() => this.updateDate('+')}><i className="fas fa-angle-right"></i></button>
+                                <button onClick={() => this.updateDayOfWeeks('+')} className="pointer"><i className="fas fa-angle-right"></i></button>
                             </td>
                         </tr>
                     </tbody>
@@ -113,7 +146,16 @@ export default class Time extends React.Component {
                 <table>
                     <tbody>
                         {
-                            this.hours.map((item, index) => <tr key={index}>{item}</tr>)
+                            this.hours.map((item, index) => (
+                                <tr key={index} className="tr-hover" >
+                                    <td>
+                                        {item}
+                                    </td>
+                                    <td>
+                                        <input type="text" className="form-control"/>
+                                    </td>
+                                </tr>
+                            ))
                         }
                     </tbody>
                 </table>
